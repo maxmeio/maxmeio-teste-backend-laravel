10b65d6de78d8b6454b8651b34daaaa83105e1ce
brendo <brendoja@gmail.com>
2022-04-01 18:05:57 -0300

    seeder

--- Prova-Brendo/database/seeders/DatabaseSeeder.php
+++ Prova-Brendo/database/seeders/DatabaseSeeder.php
@@ -49,7 +49,7 @@ class DatabaseSeeder extends Seeder
                 'user_group_id' => UserGroups::where('group_name', 'editor')->first()->id,
             ]);
         }
-        for ($i = 0; $i <= 10; $i++) {
+        for ($i = 1; $i <= 10; $i++) {
             DB::table('news')->insert([
                 'title' => 'ipsum loren' . $i,
                 'body' => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing",
